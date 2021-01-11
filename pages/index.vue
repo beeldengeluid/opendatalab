2f43b634569ac99b15a543b61effd23e96205646
Werner <werner@sudox.nl>
2021-01-11 11:28:51 +0100

    build: counter example on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6"> test </v-col>
+    <v-col cols="12" sm="8" md="6">
+      <h1>Homepage</h1>
+      <Counter />
+    </v-col>
   </v-row>
 </template>
 
 <script>
+import Counter from '../components/Counter'
+
 export default {
-  components: {},
+  components: {
+    Counter,
+  },
 }
 </script>
