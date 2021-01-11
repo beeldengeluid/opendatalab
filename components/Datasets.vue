1c1cd94c10ca0ea9b33d449851d28c49acaf827f
Werner <werner@sudox.nl>
2021-01-11 11:29:42 +0100

    build: display datasets from a local json file

---
+++ components/Datasets.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card class="mx-auto" max-width="400" tile>
+    <v-list-item two-line v-for="item in items" :key="item.id">
+      <v-list-item-content>
+        <v-list-item-title>{{ item.name }}</v-list-item-title>
+        <v-list-item-subtitle>#{{ item.records }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: { items: { type: Array, required: true } },
+}
+</script>
+
+<style scoped></style>
