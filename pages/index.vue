d30588093821cc35e85810c4141a438b683b6d9c
Werner <werner@sudox.nl>
2021-01-11 11:29:12 +0100

    build: display markdown files

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,33 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <h1>Homepage</h1>
+    <v-col cols="12">
+      <h1>{{ page.title }}</h1>
+      <v-divider class="my-5" />
       <Counter />
+      <v-divider class="my-5" />
+      <Datasets :items="datasets.items" />
+      <v-divider class="my-5" />
+      <nuxt-content :document="page" />
     </v-col>
   </v-row>
 </template>
 
 <script>
 import Counter from '../components/Counter'
+import Datasets from '../components/Datasets'
 
 export default {
   components: {
     Counter,
+    Datasets,
+  },
+  async asyncData({ $content }) {
+    const page = await $content('home').fetch()
+    const datasets = await $content('datasets').fetch()
+    return {
+      page,
+      datasets,
+    }
   },
 }
 </script>
