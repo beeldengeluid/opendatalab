220cda191ec7a187db5a1ba4df9328ed634246ed
Werner <werner@sudox.nl>
2021-01-15 19:31:15 +0100

    build: page transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~/assets/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -67,7 +67,7 @@ export default {
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['./assets/variables.scss'],
     theme: {
       dark: false,
       themes: {
