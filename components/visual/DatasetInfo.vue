670f1a0ea2bf1b9963646f591dc6e548dc7d8f58
Werner <werner@sudox.nl>
2021-02-14 16:11:21 +0100

    feat: show dataset stats

--- components/visual/DatasetInfo.vue
+++ components/visual/DatasetInfo.vue
@@ -21,6 +21,33 @@
     <h2 class="my-3">{{ dataset.title }}</h2>
     <p>{{ dataset.description }}</p>
 
+    <!-- Stats -->
+    <div
+      :style="{ fontSize: '0.8em' }"
+      class="text-uppercase grey--text lighten-4 title-font pb-4"
+    >
+      <div
+        v-for="(stat, index) of stats"
+        :key="index"
+        :style="{
+          lineHeight: '2.4em',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis',
+        }"
+      >
+        <v-icon
+          size="17"
+          color="grey lighten-4"
+          class="pb-0 mr-2"
+          v-text="stat.icon"
+        >
+        </v-icon>
+        <strong> {{ stat.text }} </strong>
+      </div>
+    </div>
+
+    <!-- Button -->
     <v-row>
       <v-col>
         <v-btn
@@ -55,6 +82,30 @@ export default {
     return {
       color: classColors.dataset,
     }
+  },
+  computed: {
+    stats() {
+      return [
+        {
+          icon: 'mdi-domain',
+          text: this.dataset.creator?.name,
+        },
+        {
+          icon: 'mdi-file-document-multiple',
+          text:
+            (this.dataset.distribution?.length
+              ? this.dataset.distribution[0].contentSize
+              : '-') +
+            ' ' +
+            this.$t('records'),
+        },
+        {
+          icon: 'mdi-calendar-range',
+          // TODO: replace with real data
+          text: '1899 - 1978',
+        },
+      ]
+    },
   },
   methods: {
     getRGBAColor,
